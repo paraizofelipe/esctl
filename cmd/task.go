4685c5dd934b89ca6b8cd88b963a423b2aa4746b
Paraizo <felipeparaizo@gmail.com>
2023-08-26 01:30:19 -0300

    fix parser arguments

--- cmd/task.go
+++ cmd/task.go
@@ -1,1 +1,42 @@
 package cmd
+
+import (
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/urfave/cli/v2"
+)
+
+func NewTaskCommand(esClient *elasticsearch.Client) *cli.Command {
+
+	appFlags := []cli.Flag{
+		&cli.BoolFlag{
+			Name:    "pretty",
+			Aliases: []string{"p"},
+			Usage:   "Format response as pretty-printed JSON",
+		},
+	}
+
+	// catManager := actions.NewTaskAction(esClient)
+
+	return &cli.Command{
+		Name:  "task",
+		Usage: "Manage Elasticsearch background tasks",
+		Flags: appFlags,
+		Subcommands: []*cli.Command{
+			{
+				Name:  "list",
+				Usage: "List all indices in Elasticsearch",
+				// Action: catManager.Indices,
+			},
+			{
+				Name:  "get",
+				Usage: "List all indices in Elasticsearch",
+				// Action: catManager.Indices,
+			},
+			{
+				Name:  "cancel",
+				Usage: "List all indices in Elasticsearch",
+				// Action: catManager.Indices,
+			},
+		},
+	}
+}
